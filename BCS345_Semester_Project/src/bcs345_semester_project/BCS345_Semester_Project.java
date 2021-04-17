3136b86d7a59b1c194b3bcca17a40b8e14047f2b
jnobile91 <jnobile91@live.com>
2021-04-16 20:39:09 -0400

    Testing some code for semester project

---
+++ BCS345_Semester_Project/src/bcs345_semester_project/BCS345_Semester_Project.java
@@ -0,0 +1,10 @@
+package bcs345_semester_project;
+
+import java.util.
+
+public class BCS345_Semester_Project {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ BCS345_Semester_Project/src/bcs345_semester_project/FXMLDocumentController.java
@@ -0,0 +1,13 @@
+package bcs345_semester_project;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLDocumentController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}

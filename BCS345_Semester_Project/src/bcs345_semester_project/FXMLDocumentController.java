ab4c170330b08c47b39db0d21b639a9df6518b51
jnobile91 <jnobile91@live.com>
2021-05-10 12:45:05 -0400

    Added Help Functionality

--- BCS345_Semester_Project/src/bcs345_semester_project/FXMLDocumentController.java
+++ BCS345_Semester_Project/src/bcs345_semester_project/FXMLDocumentController.java
@@ -346,5 +346,27 @@ private Pane canvasRoot;
         
     }
    
+    @FXML
+    private void displayReadme(ActionEvent event) {
+        String readme = "README.md";
+        String line = null;
+        try
+        {
+            FileReader fileReader = new FileReader(readme);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error reading file named '" + readme + "'");
+        }
+    }
    
 } //end of program
